ddc9beb8a4e62e09e1a05f64658b9fbecf56d382
Shammah Johnson <shamaj2006@gmail.com>
2024-08-15 16:44:15 +0100

    Added User Creation Form

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .import views
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, logoutUser
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, logoutUser, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
 path ('login/', CustomLoginView.as_view(), name='login' ),
 path('logout/', logoutUser, name='logout'),
+path('register/', RegisterPage.as_view(), name='register'),
 path ('', TaskList.as_view(), name='tasks'),
 path ('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 path ('task-create/', TaskCreate.as_view(), name='task-create'),
