2d36c928705292b68e9ef04da65cd2c066622dd2
shammah <shamaj2006@gmail.com>
2025-09-12 13:32:09 +0100

    allowd host reconfig

--- myapp/settings.py
+++ myapp/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST") 
+ALLOWED_HOSTS = ALLOWED_HOSTS = ['my2dolist.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
